c0defafe39765ae8bb1aae62d94347f5fd88c1d4
simanto789 <hazrat.ali789@gmail.com>
2023-04-11 15:18:40 +0600

    display feature job data in homepage and also mobile responsive

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -4,8 +4,19 @@ const Feature = ({feature}) => {
     const {company_logo, job_title, company_name, remote_or_onsite, fulltime_or_parttime, location, salary} = feature
     return (
         <div className='border border-gray-200 rounded-lg p-4'>
-            <img src={company_logo} alt="" />
-            <p>{company_name}</p>
+            <img className='mb-3 w-25 h-10' src={company_logo} alt="" />
+            <p className='mb-2 font-medium'>{job_title}</p>
+            <p className='mb-2'>{company_name}</p>
+            <div className='flex gap-5 mb-2'>
+                <p className='border-solid border-2 border-yellow-600 p-2'>{remote_or_onsite}</p>
+                <p className='border-solid border-2 border-yellow-600 p-2'>{fulltime_or_parttime}</p>                
+            </div>
+            <div className='flex gap-5 mb-2'>
+                <p>{location}</p>
+                <p>{salary}</p>
+            </div>
+            <button className='btn mb-5 mt-3'>View Details</button>
+            
         </div>
     );
 };
